3e56f01459a6a91c6b5fb01e0d01536d888bf72f
Shirin-yan <shirinayangibayeva@gmail.com>
2025-07-19 10:31:32 +0500

    Initial Commit

---
+++ tickerapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tickerapp
+//
+//  Created by  Shirin-Yan on 19.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
